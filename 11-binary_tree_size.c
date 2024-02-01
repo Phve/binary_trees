b312b4fa999b9d83663d2c57d66b55c3dc6e8b5f
Phve <muriujames58@gmail.com>
2024-02-01 14:10:14 -0800

    taskkk

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,7 +13,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree)
 	{
 		tr_size += 1;
-		tr_size += binary_tree_size(tree->left)
+		tr_size += binary_tree_size(tree->left);
 		tr_size += binary_tree_size(tree->right);
 	}
 	return (tr_size);
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,6 +6,7 @@
  *
  * Return: Return 0 if tree is NULL; otherwise, return the height.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
